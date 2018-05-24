23c0cba7a24b97327632cd4226a2d30dcb2f3abf
apple95 <rajaa@roe-hulman.edu>
2018-05-24 16:18:44 -0400

    Integrating with Chris's code

--- viewRenderer.js
+++ viewRenderer.js
@@ -3,6 +3,30 @@ var canvas = document.getElementById('Mycanvas');
 var context = canvas.getContext('2d');
 context.translate(0.5, 0.5);
 
+
+// var maximizeButton = {
+//   x:650,
+//   y:0,
+//   width:20,
+//   height:20
+// };
+
+// //Minimize Button identification.
+// var minimizeButton = {
+//   x:630,
+//   y:0,
+//   width:20,
+//   height:20
+// };
+
+// //Close Button identification.
+// var closeButton = {
+//   x:670,
+//   y:0,
+//   width:20,
+//   height:20
+// };
+
 // This object holds all the methods pertinent to defining and drawing the view
 var sampleView = {
 	// The dimensions to reference when drawing the view. These are dynamic and will be updated
@@ -35,7 +59,7 @@ var sampleView = {
 	// These are not mutually exclusive attributes.
 	maximized: false,
 	minimized: false,
-	open: false,
+	open: true,
 
 	/**
 	 * Updates the view's dimensions and draws the shapes
@@ -53,7 +77,7 @@ var sampleView = {
 			// draw the shapes according to the renderDimensions.
 			this.drawViewWrapper();
 			this.drawViewHead();
-			this.drawViewBody();
+			// this.drawViewBody();
 		}
 	},
 
@@ -62,14 +86,61 @@ var sampleView = {
 	 * (This will be the full dimensions of the view and draw the background and border)
 	 */
 	drawViewWrapper: function() {
-
+		context.beginPath();
+		context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y, window.innerWidth, window.innerHeight);
+		context.fillStyle = "black";
+		context.lineWidth = 2;
+		context.fill();
+		context.closePath();
 	},
 
 	/**
 	 * Draw the head content of the view (header bar, label, and buttons)
 	 */
 	drawViewHead: function() {
-		
+	// 	//Draw Maximize Button
+	// context.beginPath();
+	// context.rect(maximizeButton.x,maximizeButton.y,maximizeButton.width,maximizeButton.height); 
+	// context.rect(maximizeButton.x+5,maximizeButton.y+5,maximizeButton.width-10,maximizeButton.height-10);
+	// context.fillStyle = 'blue'; 
+	// context.lineWidth = 2;
+	// context.stroke();
+	// context.closePath();
+
+//Draw Minimize Button
+	context.beginPath();
+	context.rect(50, 50, 50, 50); 
+	context.fillStyle = "blue"; 
+	context.lineWidth = 2;
+	context.fill();
+	context.closePath();
+
+	// context.beginPath();
+	// context.moveTo(minimizeButton.x+7,minimizeButton.y+10);
+	// context.lineTo(minimizeButton.x+15,minimizeButton.y+10);
+	// context.stroke();
+	// context.closePath();
+
+// //Draw Close Button
+// context.beginPath();
+// context.moveTo(closeButton.x,closeButton.y);
+// context.lineTo(closeButton.x+20,closeButton.y+20);
+// context.stroke();
+// context.closePath();
+
+// context.beginPath();
+// context.moveTo(closeButton.x,closeButton.y+20);
+// context.lineTo(closeButton.x+20,closeButton.y);
+// context.stroke();
+// context.closePath();
+
+// context.beginPath();
+// context.rect(closeButton.x,closeButton.y,closeButton.width,closeButton.height); 
+// context.fillStyle = '#0000FF'; 
+// context.lineWidth = 2;
+// context.strokeStyle = '#000000'; 
+// context.stroke();
+// context.closePath();
 	},
 
 	/**
@@ -91,6 +162,7 @@ var sampleView = {
 		}
 		else if (this.open) {
 			// set render dimensions to customDimensions
+			//renderDimensions = customDimensions;
 		}
 	},
 
