c7a711fd88e2368c03b962e86a66099542164fea
apple95 <rajaa@roe-hulman.edu>
2018-05-28 14:43:38 -0400

    Logic for drag in progress

--- viewRenderer.js
+++ viewRenderer.js
@@ -3,8 +3,26 @@ var canvas = document.getElementById('Mycanvas');
 var context = canvas.getContext('2d');
 context.translate(0.5, 0.5);
 
+/*
+  This Function gets position of the mouse when an user clicks on the canvas.
+*/
+function getMousePos(canvas, event) {
+  var click = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - click.left,
+    y: event.clientY - click.top
+  };
+}
 
 
+  //This Function checks if the user is clicking at the cormaximizeButton buttons for respective behavior.
+
+function isInside(pos, x, y, w, h)  {
+  return pos.x > x && pos.x < x+w && pos.y < y+h && pos.y > y
+}
+
+
+//Maximize Button identification.
 // var maximizeButton = {
 //   x:650,
 //   y:0,
@@ -27,6 +45,8 @@ context.translate(0.5, 0.5);
 //   width:20,
 //   height:20
 // };
+
+
 
 // This object holds all the methods pertinent to defining and drawing the view
 var sampleView = {
@@ -55,6 +75,7 @@ var sampleView = {
 			y: 0	
 		}
 	},
+
 
 	// The view could be set to max even though it has been minimized.
 	// These are not mutually exclusive attributes.
@@ -86,15 +107,9 @@ var sampleView = {
 	},
 
 	drawCanvas: function() {
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y, window.innerWidth, window.innerHeight);
-		context.fillStyle = "black";
-		context.lineWidth = 2;
-		context.fill();
-		context.closePath();
 
 		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y+60, window.innerWidth, window.innerHeight-62);
+		context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y, window.innerWidth, window.innerHeight);
 		context.fillStyle = "gray";
 		context.lineWidth = 2;
 		context.fill();
@@ -107,19 +122,39 @@ var sampleView = {
 	 */
 	drawViewWrapper: function() {
 
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+70, this.renderDimensions.startPoint.y+300, window.innerWidth/4.5, window.innerHeight/2.5);
-		context.fillStyle = "white";
-		context.lineWidth = 2;
-		context.fill();
-		context.closePath();
 
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+70, this.renderDimensions.startPoint.y+300, window.innerWidth/4.5, window.innerHeight/2.5);
-		context.strokeStyle = "black";
-		context.lineWidth = 1;
-		context.stroke();
-		context.closePath();
+		if (this.maximized){
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y, this.renderDimensions.endPoint.x, this.renderDimensions.endPoint.y);
+			context.fillStyle = "white";
+			context.lineWidth = 2;
+			context.fill();
+			context.closePath();
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x, this.renderDimensions.startPoint.y, this.renderDimensions.endPoint.x, this.renderDimensions.endPoint.y);
+			context.strokeStyle = "black";
+			context.lineWidth = 1;
+			context.stroke();
+			context.closePath();
+
+		} else {
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+70, this.renderDimensions.startPoint.y+300, window.innerWidth/4.5, window.innerHeight/2.5);
+			context.fillStyle = "white";
+			context.lineWidth = 2;
+			context.fill();
+			context.closePath();
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+70, this.renderDimensions.startPoint.y+300, window.innerWidth/4.5, window.innerHeight/2.5);
+			context.strokeStyle = "black";
+			context.lineWidth = 1;
+			context.stroke();
+			context.closePath();
+
+
+		}
 
 	},
 
@@ -135,61 +170,122 @@ var sampleView = {
 		// context.fill();
 		// context.closePath();
 
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+75, this.renderDimensions.startPoint.y+305, 30, 30);
-		context.fillStyle = "cyan";
-		context.lineWidth = 1;
-		context.fill();
-		context.closePath();
+		if (this.maximized){
 
-		context.beginPath();
-		context.strokeStyle = "white";
-		context.moveTo(this.renderDimensions.startPoint.x+80, this.renderDimensions.startPoint.y+320);
-		context.lineTo(this.renderDimensions.startPoint.x+100, this.renderDimensions.startPoint.y+320);
-		context.stroke();
-		context.closePath();
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+5, this.renderDimensions.startPoint.y+5, this.renderDimensions.endPoint.x/26.6, this.renderDimensions.endPoint.y/26.6);
+			context.fillStyle = "cyan";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
 
-
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+107, this.renderDimensions.startPoint.y+305, 180, 30);
-		context.fillStyle = "cyan";
-		context.lineWidth = 1;
-		context.fill();
-		context.closePath();
-
-		context.beginPath();
-		context.fillStyle = "black"
-		context.font = "10px Arial";
-		context.fillText("ACL VIEW",this.renderDimensions.startPoint.x+177,this.renderDimensions.startPoint.y+320);
-		context.closePath();
-
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+289, this.renderDimensions.startPoint.y+305, 30, 30);
-		context.fillStyle = "cyan";
-		context.lineWidth = 1;
-		context.fill();
-		context.closePath();
-
-		context.beginPath();
-		context.strokeStyle = "white";
-		context.beginPath();
-		context.arc(this.renderDimensions.startPoint.x+306, this.renderDimensions.startPoint.y+320,10,0,2*Math.PI);
-		context.stroke();
-		context.closePath();
-
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+321, this.renderDimensions.startPoint.y+305, 30, 30);
-		context.fillStyle = "cyan";
-		context.lineWidth = 1;
-		context.fill();
-		context.closePath();
+			context.beginPath();
+			context.strokeStyle = "white";
+			context.moveTo(this.renderDimensions.startPoint.x+10, this.renderDimensions.startPoint.y+15);
+			context.lineTo(this.renderDimensions.startPoint.x+32, this.renderDimensions.startPoint.y+15);
+			context.stroke();
+			context.closePath();
 
 
-		context.beginPath();
-		context.strokeStyle = "white";
-		context.rect(this.renderDimensions.startPoint.x+329, this.renderDimensions.startPoint.y+313,15,15)
-		context.stroke();
-		context.closePath();
+			// context.beginPath();
+			// context.rect(this.renderDimensions.startPoint.x+107, this.renderDimensions.startPoint.y+305, 180, 30);
+			// context.fillStyle = "cyan";
+			// context.lineWidth = 1;
+			// context.fill();
+			// context.closePath();
+
+			// context.beginPath();
+			// context.fillStyle = "black"
+			// context.font = "10px Arial";
+			// context.fillText("ACL VIEW",this.renderDimensions.startPoint.x+177,this.renderDimensions.startPoint.y+320);
+			// context.closePath();
+
+			// context.beginPath();
+			// context.rect(this.renderDimensions.startPoint.x+289, this.renderDimensions.startPoint.y+305, 30, 30);
+			// context.fillStyle = "cyan";
+			// context.lineWidth = 1;
+			// context.fill();
+			// context.closePath();
+
+			// context.beginPath();
+			// context.strokeStyle = "white";
+			// context.beginPath();
+			// context.arc(this.renderDimensions.startPoint.x+306, this.renderDimensions.startPoint.y+320,10,0,2*Math.PI);
+			// context.stroke();
+			// context.closePath();
+
+			// context.beginPath();
+			// context.rect(this.renderDimensions.startPoint.x+321, this.renderDimensions.startPoint.y+305, 30, 30);
+			// context.fillStyle = "cyan";
+			// context.lineWidth = 1;
+			// context.fill();
+			// context.closePath();
+
+
+			// context.beginPath();
+			// context.strokeStyle = "white";
+			// context.rect(this.renderDimensions.startPoint.x+329, this.renderDimensions.startPoint.y+313,15,15)
+			// context.stroke();
+			// context.closePath();
+
+
+		} else{
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+75, this.renderDimensions.startPoint.y+305, 30, 30);
+			context.fillStyle = "cyan";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+
+			context.beginPath();
+			context.strokeStyle = "white";
+			context.moveTo(this.renderDimensions.startPoint.x+80, this.renderDimensions.startPoint.y+320);
+			context.lineTo(this.renderDimensions.startPoint.x+100, this.renderDimensions.startPoint.y+320);
+			context.stroke();
+			context.closePath();
+
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+107, this.renderDimensions.startPoint.y+305, 180, 30);
+			context.fillStyle = "cyan";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+
+			context.beginPath();
+			context.fillStyle = "black"
+			context.font = "10px Arial";
+			context.fillText("ACL VIEW",this.renderDimensions.startPoint.x+177,this.renderDimensions.startPoint.y+320);
+			context.closePath();
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+289, this.renderDimensions.startPoint.y+305, 30, 30);
+			context.fillStyle = "cyan";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+
+			context.beginPath();
+			context.strokeStyle = "white";
+			context.beginPath();
+			context.arc(this.renderDimensions.startPoint.x+306, this.renderDimensions.startPoint.y+320,10,0,2*Math.PI);
+			context.stroke();
+			context.closePath();
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+321, this.renderDimensions.startPoint.y+305, 30, 30);
+			context.fillStyle = "cyan";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+
+
+			context.beginPath();
+			context.strokeStyle = "white";
+			context.rect(this.renderDimensions.startPoint.x+329, this.renderDimensions.startPoint.y+313,15,15)
+			context.stroke();
+			context.closePath();
+		}
 
 	// 	//Draw Maximize Button
 	// context.beginPath();
@@ -240,12 +336,24 @@ var sampleView = {
 	 * Draw the body content of the view (this will be specific to custom views later)
 	 */
 	drawViewBody: function() {
-		context.beginPath();
-		context.rect(this.renderDimensions.startPoint.x+76, this.renderDimensions.startPoint.y+337, (window.innerWidth/4.5)- 10, (window.innerHeight/2.5)- 40);
-		context.fillStyle = "black";
-		context.lineWidth = 1;
-		context.fill();
-		context.closePath();
+
+		if(this.maximized){
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+5, this.renderDimensions.startPoint.y+51, this.renderDimensions.endPoint.x-5, this.renderDimensions.endPoint.y-51);
+			context.fillStyle = "black";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+
+		}else{
+
+			context.beginPath();
+			context.rect(this.renderDimensions.startPoint.x+76, this.renderDimensions.startPoint.y+337, (window.innerWidth/4.5)- 10, (window.innerHeight/2.5)- 40);
+			context.fillStyle = "black";
+			context.lineWidth = 1;
+			context.fill();
+			context.closePath();
+		}
 
 
 
@@ -258,9 +366,18 @@ var sampleView = {
 	setRenderDimensions: function() {
 		if (this.open && this.minimized) {
 			// set render dimensions to be minimized, relative to the canvas size
+			this.renderDimensions.startPoint.x = 0;
+			this.renderDimensions.startPoint.y = window.innerHeight-30;
+			this.renderDimensions.endPoint.x = window.innerWidth;
+			this.renderDimensions.endPoint.y = window.innerHeight;
 		}
 		else if (this.open && this.maximized) {
 			// set render dimensions to fill the canvas, relative to the current canvas size
+			this.renderDimensions.startPoint.x = 0;
+			this.renderDimensions.startPoint.y = 0;
+			this.renderDimensions.endPoint.x = window.innerWidth;
+			this.renderDimensions.endPoint.y = window.innerHeight;
+
 		}
 		else if (this.open) {
 			// set render dimensions to customDimensions
@@ -269,7 +386,7 @@ var sampleView = {
 	},
 
 	/**
-	 * Update the user-defined dimensions (usually called while the user is reisizing the view)
+	 * Update the user-defined dimensions (usually called while the user is resizing the view)
 	 */
 	setCustomDimensions: function() {
 		// Set custom dimensions to the new resized dimensions
@@ -284,6 +401,66 @@ function render() {
 	sampleView.update();
 	requestAnimationFrame(render);
 }
+
+canvas.addEventListener('click', function(evt) {
+  //debugger;
+  if (isInside(getMousePos(canvas, evt), sampleView.renderDimensions.startPoint.x+321, sampleView.renderDimensions.startPoint.y+305, 30, 30)) {
+    //drawMaximizedWindow();
+    sampleView.maximized = true;
+  }
+  // else if (isInside(mousePos,minimizeButton)) {
+  //   drawMinimizedWindow();
+  // }
+  // else if (isInside(mousePos,closeButton)) {
+  //   closeWindow();
+  // }
+}, false),
+
+/**
+DRAGGING LOGIC GOES HERE
+*/
+
+// dragElement(document.getElementById(("mydiv")));
+
+// function dragElement(elmnt) {
+//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+//   if (document.getElementById(elmnt.id + "header")) {
+//     /* if present, the header is where you move the DIV from:*/
+//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+//   } else {
+//     /* otherwise, move the DIV from anywhere inside the DIV:*/
+//     elmnt.onmousedown = dragMouseDown;
+//   }
+
+//   function dragMouseDown(e) {
+//     e = e || window.event;
+//     // get the mouse cursor position at startup:
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+//     document.onmouseup = closeDragElement;
+//     // call a function whenever the cursor moves:
+//     document.onmousemove = elementDrag;
+//   }
+
+//   function elementDrag(e) {
+//     e = e || window.event;
+//     // calculate the new cursor position:
+//     pos1 = pos3 - e.clientX;
+//     pos2 = pos4 - e.clientY;
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+//     // set the element's new position:
+//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//   }
+
+//   function closeDragElement() {
+//     /* stop moving when mouse button is released:*/
+//     document.onmouseup = null;
+//     document.onmousemove = null;
+//   }
+// }
+
 
 // Start the loop
 render();
