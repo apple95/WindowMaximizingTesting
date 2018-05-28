cdb0bb86a2c4fa6a6512b56c37ab556461450086
apple95 <rajaa@roe-hulman.edu>
2018-05-28 18:17:29 -0400

    Logic for drag in progress

--- viewRenderer.js
+++ viewRenderer.js
@@ -425,12 +425,9 @@ DRAGGING LOGIC GOES HERE
 // function dragElement(elmnt) {
 //   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 //   if (document.getElementById(elmnt.id + "header")) {
-//     /* if present, the header is where you move the DIV from:*/
+//     /* if present, the header is where you move the window from:*/
 //     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* otherwise, move the DIV from anywhere inside the DIV:*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
+//   } 
 
 //   function dragMouseDown(e) {
 //     e = e || window.event;
